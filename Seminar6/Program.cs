0dad6dc7d500dc246751aaaaafe610bde2a09dff
GabilyAslanov <aslanov.gabily@gmail.com>
2022-12-12 21:32:20 +0300

    Seminar7

---
+++ Seminar6/Program.cs
@@ -0,0 +1,84 @@
+﻿// //Напишите программу, которая перевернёт одномерный массив 
+// //(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+
+// int[] array1 = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array1));
+
+// ReversArray1(array1);
+// Console.WriteLine(String.Join(" ", array1));
+
+// array1=ReversArray2(array1);
+// Console.WriteLine(String.Join(" ", array1));
+
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+// void ReversArray1(int[] inArray)       //9 8 34 5 67 2 1
+// {
+//     for (int i = 0; i < inArray.Length / 2; i++)
+//     {
+//         int tmp = inArray[i];
+//         inArray[i] = inArray[inArray.Length - i - 1];
+//         inArray[inArray.Length - i - 1] = tmp;
+//     }
+// }
+
+// int[] ReversArray2(int[] inArray)
+// {
+//     int[] result = new int[inArray.Length];
+//     for (int i = 0; i < inArray.Length; i++)
+//     {
+//         result[i] = inArray[inArray.Length - 1 - i];
+
+//     }
+//     return result;
+// }
+
+
+//Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами
+// Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+var rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+int[,] array = GetArrayMatrix(rows, columns,10,99);
+PrintArray(array);
+
+int[,] GetArrayMatrix(int rows, int columns, int minValue=0, int maxValue=100)
+{
+    int[,] result = new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Seminar7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter nums");
+            string? str = Console.ReadLine();
+            int num = Convert.ToInt32(str);
+            System.Console.WriteLine(num);
+            if (TryParse(str, out num))
+            {
+                System.Console.WriteLine(num);
+            }
+            var tmp = new int[10,10];
+    
+        }
+
+        static bool TryParse(string str, out int num)
+        {
+            try
+            {
+                num = int.Parse(str);
+                return true;
+            }
+            catch
+            {
+                num = 0;
+                return false;
+
+            }
+
+        }
+    }
+}
